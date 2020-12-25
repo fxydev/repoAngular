e683a42ee9fc22ac05cc27331b3ac12cd9e82fe4
fxydev <ferchy99rodriguez@gmail.com>
2020-12-25 13:15:58 +0100

    v2 ngif ngfor ngmodel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//importacion de FormsModule necesaria para poder usar ngModel con radioButton
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CorreoComponent } from './correo/correo.component';
 import { ListacorreoComponent } from './listacorreo/listacorreo.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     CorreoComponent,
-    ListacorreoComponent
+    ListacorreoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
